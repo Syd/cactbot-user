f49efbf57a9f0e12d25187536609e24f50fa5f62
Scott Wisely <syd@scrimfind.org>
2018-09-29 22:37:44 +1000

    added: sunken temple doom stack reminder

--- raidboss.js
+++ raidboss.js
@@ -10,6 +10,8 @@ Options.SoundAlertsEnabled = true;
 Options.Triggers = [
     {
         ZoneRegex: /^Cutter's Cry$/, //this won't trigger in hardmodes, but I don't have them atm
+        // I tend to forget what the chimera mechanic is here because I don't go often
+        // tl;dr run away when he casts ram's voice, run close to him when he cast's dragon's voice.
         triggers: [
             {
                 id: 'Ram Voice',
@@ -30,7 +32,7 @@ Options.Triggers = [
                 regex: /The chimera begins casting The Dragon's Voice/,
                 alertText: function(data) {
                     return {
-                        en: 'run towards'  
+                        en: 'run towards'
                     };
                 },
                 tts: {
@@ -38,5 +40,25 @@ Options.Triggers = [
                 },
             }
         ] //end of the cutters cry triggers
-    } //end of the cutter's cry zone
+    }, //end of the cutter's cry zone
+
+    // Sunken temple reminders
+    {
+        ZoneRegex: /^The Sunken Temple of Qarn.*$/,
+        // don't forget the stupid doom mechanic for Teratotaur
+        triggers: [
+            {
+            id: 'doom',
+            regex: /The teratotaur uses Mortal Ray/,
+            alertText: function (data) {
+                return {
+                    en: 'run to lit panel'
+                };
+            },
+                tts: {
+                    en: 'clear doom stack'
+                },
+            },
+        ]
+    }
 ];
