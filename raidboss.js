4f4e5e9a3606b311ac905c952d9174648ac3177b
Scott Wisely <syd@scrimfind.org>
2018-09-29 19:23:24 +1000

    added: Cutter's cry normal

--- raidboss.js
+++ raidboss.js
@@ -1,3 +1,33 @@
 Options.Triggers = [
+    {
+        ZoneRegex: /^Cutter's Cry$/, //this won't trigger in hardmodes, but I don't have them atm
+        triggers: [
+            {
+                id: 'Ram Voice',
+                regex: /:The chimera begins casting the Ram's voice/,
 
+                alertText: function(data) {
+                    return {
+                        en: 'run away'
+                    };
+                },
+                tts: {
+                    en: 'run away'
+                },
+            },
+
+            {
+                id: 'Dragon voice',
+                regex: /The chimera begins casting The Dragon's Voice/,
+                alertText: function(data) {
+                    return {
+                        en: 'run towards'  
+                    };
+                },
+                tts: {
+                    en: 'run towards'
+                },
+            }
+        ] //end of the cutters cry triggers
+    } //end of the cutter's cry zone
 ];
